f9758008fb649ecd43c23d1a189d66017cc430fb
richanynguon <alina.nguon@gmail.com>
2019-10-22 17:33:13 -0400

    Created Basic Actions that Corresponds with API

--- src/redux/events/eventsActions.js
+++ src/redux/events/eventsActions.js
@@ -3,3 +3,4 @@
 
 // // Helpers
 // import { UseAxiosWithAuth } from "../../helpers/auth";
+
--- src/redux/users/usersActions.js
+++ src/redux/users/usersActions.js
@@ -2,4 +2,39 @@
 // import * as types from "./userTypes";
 
 // // Helpers
-// import { UseAxiosWithAuth } from "../../helpers/auth";
+import { UseAxiosWithAuth } from "../../helpers/auth";
+
+const APIURL = 'https://potluck-backend.herokuapp.com/';
+
+export const CreateUsers = id => dispatch => { 
+  UseAxiosWithAuth()
+  .put(APIURL + `/api/users/${id}`)
+  .then(res=>{
+    dispatch(action(res.data))
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+export const EditUser = id => dispatch => {
+  UseAxiosWithAuth()
+  .put(APIURL + `/api/users/${id}`)
+  .then(res=>{
+    dispatch(action(res.data))
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+export const DeleteUser = id => dispatch => {
+  UseAxiosWithAuth()
+  .delete(APIURL + `/api/users/${id}`)
+  .then(res=>{
+    dispatch(action(res.data))
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
