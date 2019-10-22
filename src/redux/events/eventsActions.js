62d769621fa6771fbb689fed267c02d2e5a43f89
richanynguon <alina.nguon@gmail.com>
2019-10-22 18:18:23 -0400

    Created Basic Actions that Corresponds with API

--- src/redux/events/eventsActions.js
+++ src/redux/events/eventsActions.js
@@ -4,3 +4,37 @@
 // // Helpers
 import { UseAxiosWithAuth } from "../../helpers/auth";
 
+const APIURL = "https://potluck-backend.herokuapp.com/";
+
+export const CreateEvent = id => dispatch => {
+	UseAxiosWithAuth()
+		.post(APIURL + `/api/events/${id}`)
+		.then(res => {
+      dispatch(action(res.data))
+    })
+		.catch(err => {
+      console.log(err)
+    });
+};
+
+export const EditEvent = id => dispatch => {
+  UseAxiosWithAuth()
+  .put(APIURL + `/api/events/${id}`)
+  .then(res=>{
+    dispatch(action(res.data))
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+export const DeleteEvent = id => dispatch => {
+  UseAxiosWithAuth()
+  .delete(APIURL + `/api/events/${id}`)
+  .then(res=>{
+    dispatch(action(res.data))
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
