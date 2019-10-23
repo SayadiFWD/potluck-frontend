fb2a04a9fa5b1f81f6e1e42f711ae516389a6c81
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-23 15:35:07 +0000

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,35 @@
 // Libraries
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Form, withFormik, Field } from 'formik';
-import axios from 'axios';
-import * as Yup from 'yup';
+import React from "react";
+import { Form, withFormik, Field } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
 
 const Login = ({ errors, touched, values, status }) => {
-	return (
-    <Form className='sl-form'>
-      <div className='field is-large'>
-        <Field className='control is-large' type='text' name='username' placeholder='Username'/>
+  return (
+    <Form className="sl-form">
+      <div className="field is-large">
+        <Field
+          className="control is-large"
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
         {touched.username && errors.username && (
           <p className="error">{errors.username}</p>
         )}
       </div>
-      <div className='field'>
-        <Field className='control' type='password' name='password' placeholder='Password'/>
+      <div className="field">
+        <Field
+          className="control"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
         {touched.password && errors.password && (
           <p className="error">{errors.password}</p>
         )}
       </div>
-      <button className='button is-link'>Log In</button>
+      <button className="button is-link">Log In</button>
     </Form>
   );
 };
@@ -28,25 +37,26 @@ const Login = ({ errors, touched, values, status }) => {
 const LoginForm = withFormik({
   mapPropsToValues({ username, password }) {
     return {
-      username: username || '',
-      password: password || ''
-    }
+      username: username || "",
+      password: password || ""
+    };
   },
 
   validationSchema: Yup.object().shape({
-    username: Yup.string().required('You need a username'),
-    password: Yup.string().required,
+    username: Yup.string().required("You need a username"),
+    password: Yup.string().required
   }),
 
   handleSubmit(values, { setStatus, resetForm, props }, history) {
-    resetForm()
-    axios.post('https://reqres.in/api/users/', values)
+    resetForm();
+    axios
+      .post("https://reqres.in/api/users/", values)
       .then(res => {
-        setStatus(res.data)
-        props.history.push('/dashboard') //or should be users/:id/dashboard?
+        setStatus(res.data);
+        props.history.push("/dashboard"); //or should be users/:id/dashboard?
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
-})(Login)
+})(Login);
 
-export default withRouter(LoginForm);
+export default LoginForm;
