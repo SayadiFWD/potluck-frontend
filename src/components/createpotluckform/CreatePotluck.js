e2b0d560bef91241750e62af648c44345b9c45cd
Rasha Moumneh <rasha@fastmail.com>
2019-10-24 20:01:45 -0400

    Apply styling to sign up and first form page

--- src/components/createpotluckform/CreatePotluck.js
+++ src/components/createpotluckform/CreatePotluck.js
@@ -5,41 +5,44 @@ import * as Yup from 'yup';
 
 const CreatePotluck = () => {
 	return (
-    <Form>
-      <div className="field is-large">
-        <Field 
-          className='control'
-          name='event_name' 
-          type='text' 
-          placeholder='Event Name'
-        />
-      </div>
-      <div className="field is-large">
-        <Field 
-          className='control'
-          name='dates' 
-          type='text' 
-          placeholder='Date'
-        />
-      </div>
-      <div className="field is-large">
-        <Field 
-          className='control'
-          name='time' 
-          type='text' 
-          placeholder='Time'
-        />
-      </div>
-      <div className="field is-large">
-        <Field 
-          className='control'
-          name='address' 
-          type='text' 
-          placeholder='Address'
-        />
-      </div>
-      <button className="button is-link">Next</button>
-    </Form>
+    <div className='container'>
+      <h1 className='title is-3'>Let's get some basic information first.</h1>
+      <Form className='potluckform'>
+        <div className="field is-large">
+          <Field 
+            className='control'
+            name='event_name' 
+            type='text' 
+            placeholder='Give your event a name'
+          />
+        </div>
+        <div className="field is-large">
+          <Field 
+            className='control'
+            name='dates' 
+            type='text' 
+            placeholder='What date will the event be?'
+          />
+        </div>
+        <div className="field is-large">
+          <Field 
+            className='control'
+            name='time' 
+            type='text' 
+            placeholder='What time?'
+          />
+        </div>
+        <div className="field is-large">
+          <Field 
+            className='control'
+            name='address' 
+            type='text' 
+            placeholder='Where?'
+          />
+        </div>
+        <button className="button next">Next</button>
+      </Form>
+    </div>
   );
 };
 
